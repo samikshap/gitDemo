c7ffbe76e1847af74538c3ec0f25a82ce43aa4ed
samiksha patil <samikshap@cybage.com>
2017-03-09 16:39:32 +0530

    committ done

---
+++ src/main/java/Model/Student.java
@@ -0,0 +1,22 @@
+package Model;
+
+/**
+ * Created by Administrator on 3/9/2017.
+ */
+public class Student {
+    int id;
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/main/java/com/cybage/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.cybage.controller;
+
+/**
+ * Created by Administrator on 3/9/2017.
+ */
+public class HomeController {
+
+  public void display()
+  {
+
+      System.out.println("hello samiksha");
+  }
+}
